e8fb01dfe9c3609aac073e0aadd220075605fc99
inceabdullah <pax.castrensis@gmail.com>
2019-12-01 23:50:51 +0300

    app.tsx fixed

--- App.tsx
+++ App.tsx
@@ -1,87 +1,111 @@
-### installation
+import React from 'react';
+import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
-- This is belonging to [invertase](https://github.com/invertase/)
-I used version 5.4.0 of ***react-native-firebase-starter***
-`git clone https://github.com/invertase/react-native-firebase-starter.git`
+import firebase from 'react-native-firebase';
 
-- A bunch of code of [Katharine Padilha](https://gist.github.com/katharinepadilha/d82502b4abbe6be3a244783ff5b8652e) was  used to get notifications on Android.
+export default class App extends React.Component {
 
-		async getToken() {
-    	let fcmToken = await AsyncStorage.getItem('fcmToken');
-    	if (!fcmToken) {
+  constructor(prop) {
+    super(prop);
+    this.state = {};
+  }
+
+  async getToken() {
+    let fcmToken = await AsyncStorage.getItem('fcmToken');
+    console.log("token: " + fcmToken)
+    if (!fcmToken) {
         fcmToken = await firebase.messaging().getToken();
         if (fcmToken) {
             await AsyncStorage.setItem('fcmToken', fcmToken);
-    	    	}
-	    	}
-		}
+        }
+    }
+  }
+  
+  async checkPermission() {
+    const enabled = await firebase.messaging().hasPermission();
+    console.log("after check")
+    if (enabled) {
+        this.getToken();
+    } else {
+        this.requestPermission();
+    }
+  }
+  
+  async requestPermission() {
+    try {
+        await firebase.messaging().requestPermission();
+        this.getToken();
+    } catch (error) {
+        console.log('permission rejected');
+    }
+  }
+  
+  async createNotificationListeners() {
+    firebase.notifications().onNotification(notification => {
+        notification.android.setChannelId('insider').setSound('default')
+        firebase.notifications().displayNotification(notification)
+    });
+  }
+  
+  componentDidMount() {
+    console.log("start diidmount")
+    const channel = new firebase.notifications.Android.Channel('insider', 'insider channel', firebase.notifications.Android.Importance.Max)
+    console.log("after channel..")
+    firebase.notifications().android.createChannel(channel);
+    console.log("after not creating..")
+    this.checkPermission();
+    console.log("after check..")
+    this.createNotificationListeners();
+  }
+  
 
-		async checkPermission() {
-    		const enabled = await firebase.messaging().hasPermission();
-    		if (enabled) {
-        		this.getToken();
-    		} else {
-        		this.requestPermission();
-    		}
-		}
+  render() {
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          
+          <Text>Just text</Text>
+        </View>
+      </ScrollView>
+    );
+  }
+}
 
-		async requestPermission() {
-    		try {
-        		await firebase.messaging().requestPermission();
-        		this.getToken();
-    		} catch (error) {
-        		console.log('permission rejected');
-    		}
-		}
-
-		async createNotificationListeners() {
-    		firebase.notifications().onNotification(notification => {
-        		notification.android.setChannelId('insider').setSound('default')
-        		firebase.notifications().displayNotification(notification)
-    		});
-		}
-
-		componentDidMount() {
-    				const channel = new firebase.notifications.Android.Channel('insider', 'insider channel', firebase.notifications.Android.Importance.Max)
-    		firebase.notifications().android.createChannel(channel);
-    		this.checkPermission();
-    		this.createNotificationListeners();
-		}
-		
-		
-
-
-- I have used typecript for developing my React Native projects. So, I had to migrate my any React Native project to TypeScript. For this, I used the way [Michał Zamorski](https://itnext.io/migrating-a-react-native-app-to-typescript-21f3a3d5ff57) gave.:
-
-That is like:
-`npm i --save-dev react-native-typescript-transformer typescript tslib ts-jest`
-`npm uninstall --save-dev babel-eslint eslint eslint-plugin-react`
-`npm uninstall --save prop-types`
-`npm install --save-dev @types/jest @types/react @types/react-native @types/react-navigation @types/react-redux @types/react-test-renderer`
-`npm i -g typescript tslint`
-`tslint --init`
-`tslint -p tsconfig.json`
-
-Then, Sure thing. I created specific google-services.json file for my package name: `com.invertase.rnfirebasestarter`
-
-That is it.
-
-### Some Cautions
-
-You would get caution about AsyncStorage from react native core bla bla.
-for solving you should install `@react-native-community/async-storage` and use it.
-like: `yarn add @react-native-community/async-storage`
-
-in App.js added the line below:
-`import AsyncStorage from '@react-native-community/async-storage';`
-
-At the end of installation, i changed the extention of the file name App.js to tsx.
-
-
-
-
-
-### Push Notification
-
-i could get push notification with image even my react native version is 0.61
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  logo: {
+    height: 120,
+    marginBottom: 16,
+    marginTop: 64,
+    padding: 10,
+    width: 135,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  modules: {
+    margin: 20,
+  },
+  modulesHeader: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  module: {
+    fontSize: 14,
+    marginTop: 4,
+    textAlign: 'center',
+  }
+});
